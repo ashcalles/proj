0636c7832170134295877db7d1b9144450fb6430
ashcalles <acalles1@depaul.edu>
2023-02-03 17:47:39 -0600

    m

--- students/models.py
+++ students/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
 
     def __str__(self) -> str:
