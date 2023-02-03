1e15dc4e5d9293057408bed8799e883be789b4c2
ashcalles <acalles1@depaul.edu>
2023-02-03 16:47:03 -0600

    djangosetup5

--- homework/settings.py
+++ homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2t@)by2qfi2=_y3*0$b!(tl4e7%27f^ar=ynj!7_q4ata@x$w2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
